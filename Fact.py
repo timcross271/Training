d5532a91878f9e46fd685588af78657946571d45
timcross271 <timcross271@gmail.com>
2024-05-24 19:16:32 +0100

    latest changes

--- Fact.py
+++ Fact.py
@@ -1,8 +1,34 @@
-#Factorial
+#Factorial - Done
+#Make a function, then integrate with other codes - done
 
-#Integer verification
-#Make a function, then integrate with other codes
 
-#Search within a list
-#If it is in the list or not, and what position
 
+
+
+
+
+def mult (a,b):
+    mult_result=0
+    counter=0
+    if b>a:
+        c=b
+        b=a
+        a=c
+    while counter<b:
+        mult_result=mult_result+a
+        counter=counter+1
+    return mult_result
+
+val1=int(input("Enter number:"))
+a=val1
+b=val1
+c=val1
+
+for p in range(0,c-1):
+    a=a-1
+    b=mult(b,a)
+
+if b==0:
+    b=b+1
+
+print(b)
---
+++ Search.py
@@ -0,0 +1,6 @@
+#Search within a list
+#If it is in the list or not, and what position
+
+#Find the Difference in Age between Oldest and Youngest Family Members
+
+#Integer verification
