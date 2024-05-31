2fb0dc5e197e3427c25cb50a1935ea3d8f459530
timcross271 <timcross271@gmail.com>
2024-05-31 20:33:24 +0100

    Age code

---
+++ Dice_Prob.py
@@ -0,0 +1,2 @@
+#Calulate dice probability for an n-sided dice. And then if the dice is rolled multiple times
+
--- Projects.py
+++ Projects.py
@@ -1,3 +1,15 @@
-#Find the Difference in Age between Oldest and Youngest Family Members
+#Find the Difference in Age between Oldest and Youngest Family Members - Done
 
 #Integer verification
+
+#Mean of a list of values - Done
+
+#Standard deviation of a list of values - Done
+
+#Calulate dice probability for an n-sided dice. And then if the dice is rolled multiple times
+
+#Calculate the probability for randomly choosing an item out of a list of N-items
+ 
+#Covariance between two lists of values (Coralation between lists)
+
+#Markov Chain
