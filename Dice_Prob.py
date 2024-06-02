126c1132375bbbe32e02ad243c52a0c58ecd89b9
timcross271 <timcross271@gmail.com>
2024-06-02 11:54:23 +0100

    Dice code

--- Dice_Prob.py
+++ Dice_Prob.py
@@ -1,2 +1,10 @@
 #Calulate dice probability for an n-sided dice. And then if the dice is rolled multiple times
 
+#1 in 6 probability
+
+sides=int(input('Please input number of sides on dice '))
+rolls=int()
+
+probability=0
+probability=round((1/sides)*100,2)
+print(probability,'%')
