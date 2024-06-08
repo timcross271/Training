ad62c2a32a86e630900a353c3b17cb95f266356e
timcross271 <timcross271@gmail.com>
2024-06-08 15:03:36 +0100

    Dice code

--- Dice_Prob.py
+++ Dice_Prob.py
@@ -3,8 +3,13 @@
 #1 in 6 probability
 
 sides=int(input('Please input number of sides on dice '))
+number_dice=int(input('Please input number of dice '))
 rolls=int()
 
 probability=0
+probability_mutiple=0
 probability=round((1/sides)*100,2)
-print(probability,'%')
+probability_mutiple=round((1/sides**number_dice)*100,2)
+
+print('Probability of 1 dice',probability,'%')
+print('Probability of',number_dice,'dice ',probability_mutiple,'%')
