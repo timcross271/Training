d2c2e527d17b5fbce6d6dc7dddb574d9d889a940
timcross271 <timcross271@gmail.com>
2024-07-14 19:28:33 +0100

    Chi Square Update

--- Chi_Squared.py
+++ Chi_Squared.py
@@ -5,9 +5,30 @@ input_data=np.loadtxt('data_for_linear_regression.txt')
 
 #Define range of bins
 
-#Find max of x and f(x)
-for number in input_data:
+#Find max and min of x
+max=input_data[0,0]
+for number in input_data[:,0]:
+    if number>=max:
+        max=number
+print(max)
 
+min=input_data[0,0]
+for number in input_data[:,0]:
+    if number<=min:
+        min=number
+print(min)
+
+bin_number=int(input('Please enter number of bins '))
+bin_size=(max-min)/bin_number
+print(bin_size)
+
+for p in range(bin_number):
+    bin[p]=min+bin_size
+
+#Edge list or array creation
+    
+    
+    
 
 
 
